d280a5cfa91fe1c8676259a284468a4888c0a3db
amansour13 <a.mansour.dev1@gmail.com>
2023-09-23 00:16:07 +0300

    a new version of task 1 submit all buttons functionality done and ui is fine.
    there are 2 issues i have in mind now number 1 is i need to make a fixed size to the upper section and main frame, and number 2 is find another way to delete Task without using destroy or grid_forget (find a better way) as it gives me an error when the program shut down

--- to_do_list_task1.py
+++ to_do_list_task1.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 import tkinter as tk
 first_color, second_color, third_color, forth_color = "#2C3333", "#2E4F4F", "#0E8388", "#CBE4DE"
-
+all_tasks = []
+edit_task_index = -1
 
 #functions or classes used in the program
 class HoverButton(tk.Button):
@@ -21,10 +22,76 @@ class HoverButton(tk.Button):
         self['background'] = self.defaultBackground
         self['foreground'] = self.defaultForeground
 
+class TaskWidget:
+    ctr_tasks = 0
+    txt = ""
+    def __init__(self, parent_frame, task_text):
+        self.task_frame = Frame(parent_frame, bg=third_color, height=60, highlightbackground=forth_color, highlightthickness=2)
+        self.task_frame.grid(row=self.ctr_tasks + 4, column=0, columnspan=3 , sticky="w", pady=5)
+
+        self.task_label = Label(self.task_frame, wraplength=750, anchor="w", text=task_text, bg=third_color, fg=second_color, font=('Helvetica', 15), pady=5, width=65)
+        self.task_label.grid(row=0, column=0, sticky=W)
+
+        self.edit_button = HoverButton(self.task_frame, text="edit", relief="flat", padx=10, bg='darkgreen', fg=forth_color, activebackground=forth_color, activeforeground=second_color, pady=5)
+        self.edit_button.grid(row=0, column=1, sticky=E, padx=5)
+        self.edit_button.config(command=self.edit)
+
+        self.delete_button = HoverButton(self.task_frame, text="delete", relief="flat", padx=10, bg='darkred', fg=forth_color, activebackground=forth_color, activeforeground=second_color, pady=5)
+        self.delete_button.grid(row=0, column=2, sticky=E)
+        self.delete_button.config(command=self.__del__)
+        TaskWidget.ctr_tasks += 1
+        self.txt = task_text
+
+    def get_task_text(self):
+        return self.txt
+    
+    def edit(self):
+        new_task_en.delete(0, END)
+        new_task_en.insert(0, self.txt)
+        global edit_task_index
+        edit_task_index = all_tasks.index(self)
+        submit_btn.config(text="EDIT")
+    
+    def change_task_text(self, new_text):
+        self.txt = new_text
+        self.task_label.config(text=new_text)
+
+    def __del__(self):
+        for i in all_tasks:
+            if (i.get_task_text() == self.txt):
+                i.task_frame.grid_forget() # this line where is the error occurs
+                all_tasks.remove(i)
+                break
+        TaskWidget.ctr_tasks -= 1
+
+def add_new_task():
+    task_txt = new_task_en.get()
+    all_tasks.append(TaskWidget(main_frame, task_txt))
+    new_task_en.delete(0, END)
+    
+def edit_task():
+    all_tasks[edit_task_index].change_task_text(new_task_en.get())
+    new_task_en.delete(0, END)
+    submit_btn.config(text="submit")
+
+
+def btn_functions():
+    if (submit_btn.cget("text") == "submit"):
+        add_new_task()
+    else:
+        edit_task()
+
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
+
 # initialize the form
 main = tk.Tk()
 main.title("ToDo List App")
-main.geometry("1000x600")
+center_window(main, 1000, 600)
 
 # set upper_section or title's section
 upper_section = PanedWindow(bg=second_color, height=1)
@@ -44,8 +111,9 @@ new_task_en.grid(row=1, column=0, sticky="w")
 
 submit_btn = HoverButton(main_frame, text="submit", relief="flat", padx=10, bg=third_color, fg=forth_color, activebackground=forth_color, activeforeground=second_color)
 submit_btn.grid(row=1, column=1, sticky="w", padx=10)
+submit_btn.config(command=btn_functions)
 
-tasks_title = Label(main_frame, text="Tasks", bg=first_color, fg=forth_color, font=('Helvetica', 20, 'bold'), pady=30)
+tasks_title = Label(main_frame, text="Tasks", bg=first_color, fg=forth_color, font=('Helvetica', 20, 'bold'), pady=20)
 tasks_title.grid(row=2, column=0, sticky="nw")
 
 
