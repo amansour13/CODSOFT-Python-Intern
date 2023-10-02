537079b7f009c58bd0b341b3f4fb0d6b0c0e7c7d
amansour13 <a.mansour.dev1@gmail.com>
2023-10-02 18:41:44 +0300

    another version scrollbar solved

--- to_do_list_task1.py
+++ to_do_list_task1.py
@@ -51,17 +51,19 @@ class TaskWidget:
     txt = ""
     def __init__(self, parent_frame, task_text):
         self.task_frame = Frame(parent_frame, bg=third_color, height=60, highlightbackground=forth_color, highlightthickness=1)
-        self.task_frame.grid(row=self.ctr_tasks + 4, column=0, columnspan=3 , sticky="w", pady=5)
+        self.task_frame.pack(side="top", fill='both',padx=5, pady=5)
         self.task_label = Label(self.task_frame, wraplength=750, anchor="w", text=task_text, bg=third_color, fg=second_color, font=('Helvetica', 15), pady=5, width=68)
-        self.task_label.grid(row=0, column=0, sticky=W)
-
-        self.edit_button = HoverButton(self.task_frame, text="edit", relief="flat", padx=10, bg='darkgreen', fg=forth_color, activebackground=forth_color, activeforeground=second_color, pady=5)
-        self.edit_button.grid(row=0, column=1, sticky=E, padx=5)
-        self.edit_button.config(command=self.edit)
+        self.task_label.pack(side="left")
 
         self.delete_button = HoverButton(self.task_frame, text="delete", relief="flat", padx=10, bg='darkred', fg=forth_color, activebackground=forth_color, activeforeground=second_color, pady=5)
-        self.delete_button.grid(row=0, column=2, sticky=E)
+        self.delete_button.pack(side='right')
         self.delete_button.config(command=self.delete)
+
+        self.edit_button = HoverButton(self.task_frame, text="edit", relief="flat", padx=10, bg='darkgreen', fg=forth_color, activebackground=forth_color, activeforeground=second_color, pady=5)
+        self.edit_button.pack(side='right', padx=5)
+        self.edit_button.config(command=self.edit)
+
+        
         TaskWidget.ctr_tasks += 1
         self.txt = task_text
 
@@ -82,7 +84,7 @@ class TaskWidget:
     def delete(self):
         for i in all_tasks:
             if (i.get_task_text() == self.txt):
-                i.task_frame.grid_remove()
+                i.task_frame.pack_forget()
                 all_tasks.remove(i)
                 break
     def __del__(self):
@@ -127,33 +129,35 @@ title.grid(row=0, column=0, padx=400, pady=20)
 main_frame = Frame(main, bg=first_color, padx=50)
 main_frame.pack(fill=BOTH, expand=1)
 
-add_item_title = Label(main_frame, text="Add Items", bg=first_color, fg=forth_color, font=('Helvetica', 20, 'bold'), pady=30)
-add_item_title.grid(row=0, column=0, sticky="nw")
+add_item_title = Label(main_frame, text="Add Items", bg=first_color, fg=forth_color, font=('Helvetica', 20, 'bold'), pady=10)
+add_item_title.pack(side='top', anchor='w')
 
-new_task_en = Entry(main_frame, width=70, font=('Helvetica', 15), relief="flat", fg=second_color)
-new_task_en.grid(row=1, column=0, sticky="w")
+message = Label(main_frame, text="testtest", bg=first_color, fg="lightgreen", font=('Helvetica', 8, 'bold'), pady=2)
+message.pack(side='top', anchor='w')
 
-submit_btn = HoverButton(main_frame, text="submit", relief="flat", padx=10, bg=third_color, fg=forth_color, activebackground=forth_color, activeforeground=second_color)
-submit_btn.grid(row=1, column=1, sticky="w", padx=10)
+entry_button_frame = Frame(main_frame, bg=first_color)
+entry_button_frame.pack(side='top', anchor='w')
+
+new_task_en = Entry(entry_button_frame, width=70, font=('Helvetica', 15), relief="flat", fg=second_color)
+new_task_en.pack(side='left', padx=5, pady=5, anchor="w")
+
+submit_btn = HoverButton(entry_button_frame, text="submit", relief="flat", padx=10, bg=third_color, fg=forth_color, activebackground=forth_color, activeforeground=second_color)
+submit_btn.pack(side='left', padx=10)
 submit_btn.config(command=btn_functions)
 
 tasks_title = Label(main_frame, text="Tasks", bg=first_color, fg=forth_color, font=('Helvetica', 20, 'bold'), pady=20)
-tasks_title.grid(row=2, column=0, sticky="nw")
+tasks_title.pack(side='top', anchor='w')
 
-# Create a frame to contain the scrollable content inside main_frame
-scrollable_frame = tk.Frame(main_frame)
-scrollable_frame.grid(row=3, column=0, columnspan=2, sticky="ew")
 
-# Configure the columns to expand horizontally
-main_frame.columnconfigure(0, weight=1)
-main_frame.columnconfigure(1, weight=1)
+test = Frame(main_frame)
+test.pack(side="top", fill="x")
 
 # Create a canvas widget inside the scrollable_frame
-canvas = tk.Canvas(scrollable_frame, bg=first_color, relief='flat', highlightthickness=0)
+canvas = tk.Canvas(test, bg=first_color, relief='flat', highlightthickness=0)
 canvas.pack(fill="both", expand=True, side="left")
 
 # Create a vertical scrollbar for the canvas
-scrollbar = ttk.Scrollbar(scrollable_frame, orient="vertical", command=canvas.yview)
+scrollbar = ttk.Scrollbar(test, orient="vertical", command=canvas.yview)
 scrollbar.pack(side="right", fill="y", pady=0)
 scrollbar['takefocus'] = 0
 # Configure the canvas to work with the scrollbar
@@ -173,8 +177,11 @@ canvas.bind_all("<MouseWheel>", on_mousewheel)
 def configure_scrollable_frame(event):
     canvas.configure(scrollregion=canvas.bbox("all"))
 
-scrollable_frame.bind("<Configure>", configure_scrollable_frame)
+scrollable_content_frame.bind("<Configure>", configure_scrollable_frame)
+
+# Create a frame to contain additional content below the scrollbar
+space_frame = Frame(main_frame, bg=first_color, pady=0)
+space_frame.pack(side="bottom", anchor="w")
 
 # ready to run the application after set all widgets
 main.mainloop()
-
